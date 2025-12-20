d17fef50362a104eb3ec12fcafa9bbc92fc9ee0f
Rohit Saha <103872396+irohitsaha@users.noreply.github.com>
2025-12-20 22:42:23 +0530

    container config

---
+++ 01_Basics/04_Operation.js
@@ -0,0 +1,26 @@
+let value = 3;
+let negValue = -value;
+
+console.log(value);
+console.log(negValue);
+
+let str1 = "Hello";
+let str2 = " Rohit";
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1"+2); //12
+console.log("1" +2+2); //122
+console.log(1+2+"2"); //32
+
+console.log(+true);
+console.log(+"");
+
+let n1,n2,n3;
+n1 = n2 = n3 = 2+2;
+console.table([n1,n2,n3]);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); // 101
+console.log(++gameCounter); //102
