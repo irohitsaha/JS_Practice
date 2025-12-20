a692721d9bf973ec61827ba6bf37bb44ebae13c1
Rohit Saha <103872396+irohitsaha@users.noreply.github.com>
2025-12-20 22:03:09 +0530

    Add files via upload

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //! treat all JS code as newer version
+
+// alert(3+3); //pop-up
+
+console.log(3+3);
+
+// console.log(3+3) console.log("hii"); //error
+
+let name = "Rohit";
+let age = 18;
+let isLoggedIn = false;
+
+//! primitive
+//? number = 2^53
+//? bigint
+//? string = " "
+//? boolean = true/false
+//? null = standalone value
+//? undefined = right now i didn't add value, in future maybe I add
+//? symbol = unique
+
+//? object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
