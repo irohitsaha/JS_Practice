2c918abb8401e0164b019df872caaa15b8bf3c43
Rohit Saha <103872396+irohitsaha@users.noreply.github.com>
2025-12-20 22:49:22 +0530

    container config

---
+++ 01_Basics/05_Comparision.js
@@ -0,0 +1,16 @@
+console.log(2>1);
+console.clear()
+
+console.log("2">1);
+console.log("02">1);
+console.clear()
+
+console.log(null>0); //false
+console.log(null>0); //false
+console.log(null>=0); //true
+console.clear()
+
+console.log(undefined>0); //false
+console.log(undefined>0); //false
+console.log(undefined>=0); //false
+console.log(undefined<=0); //false
