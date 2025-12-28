bbf2b02c5e59370ceb3c117c0374f019cc4f77e4
Rohit Saha <103872396+irohitsaha@users.noreply.github.com>
2025-12-28 14:41:37 +0530

    container config

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,22 @@
+const arr = [0,2,5,3,5,2];
+console.log(arr);
+console.log(arr[0]);
+
+console.clear();
+ 
+arr.push(6);
+console.log(arr);
+
+arr.push(7);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+arr.unshift(82);
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+console.log(arr.includes(3));
